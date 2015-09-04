e24d336936219a3033add6a9c516f4d94f9eee92
Liuminosity <liu.stanford@gmail.com>
2015-09-03 21:34:02 -0700

    (feat) added foursquare api call and server response data structure

--- apiKeys_example.js
+++ apiKeys_example.js
@@ -2,12 +2,19 @@
 //gitignore
 
 
-module.exports.yelpKeys = function(){
-    return {
-      consumer_key: "YELP_KEY", 
-      consumer_secret: "YELP_SECRET",
-      token: "YELP_TOKEN",
-      token_secret: "YELP_TOKEN_SECRET"
-    };
+module.exports.yelpKeys = function() {
+  return {
+    consumer_key: "YELP_KEY", 
+    consumer_secret: "YELP_SECRET",
+    token: "YELP_TOKEN",
+    token_secret: "YELP_TOKEN_SECRET"
+  };
 };
 
+
+module.exports.foursquareKeys = function() {
+  return {
+    client_ID: '4SQKEY',
+    client_secret: '4SQSECRET'
+  };
+};
--- utils.js
+++ utils.js
@@ -1,6 +1,7 @@
 //API search functions
 
 var apiKeys = require('./apiKeys.js');
+var request = require("request");
 var yelp = require("yelp").createClient(apiKeys.yelpKeys());
 
 module.exports.yelpSearch = function(searchTerm, callback){
@@ -8,3 +9,59 @@ module.exports.yelpSearch = function(searchTerm, callback){
     callback(data);
   });
 };
+
+module.exports.foursquareSearch = function(searchTerm, callback) {
+
+  //this is the query string to be passed into foursquare's server
+  var queryString = 
+    'https://api.foursquare.com/v2/venues/explore?client_id=' 
+    + apiKeys.foursquareKeys().client_ID + 
+    '&client_secret=' + apiKeys.foursquareKeys().client_secret + 
+    '&v=20130815&' +
+    'near=' + searchTerm.location +
+    '&query=' + searchTerm.term;
+
+  request(queryString, function(error, response, body) {
+    if (error) {
+      throw error;
+    }
+    parsedBody = JSON.parse(body);
+    bodyDir = parsedBody.response.groups[0]
+    var rest = new Restaurant(
+      bodyDir.items[0].venue.name,
+      bodyDir.items[0].venue.location.address,
+      bodyDir.items[0].venue.url,
+      bodyDir.items[0].venue.location.lat,
+      bodyDir.items[0].venue.location.lng,
+      'yelpData',
+      {rating: bodyDir.items[0].venue.rating,
+       url: 'TODO',
+       reviewCount: bodyDir.items[0].venue.ratingSignals
+      });
+    callback(rest);
+  })
+
+}
+
+var Restaurant = function(name, address, url, lat, long, yelpData, foursquareData) {
+  this.name = name;
+  this.address = address;
+  this.url = url;
+  this.location = {
+    latitute: lat,
+    longitude: long
+  };
+  this.yelpData = 'asdf';
+  // {
+  //   yelpRatingImage: 'https://www.yelp.com/image',
+  //   url: 'https://www.yelp.com',
+  //   reviewCount: 123
+  // }
+  this.foursquareData = foursquareData;
+  // {
+  //   rating: 9.8,
+  //   url: 'https://www.foursquare.com',
+  //   reviewCount: 23
+  // },
+  this.compositeScore = 'TODO';
+}
