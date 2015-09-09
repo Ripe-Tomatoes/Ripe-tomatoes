3c4d1bd6843f536b67d9271d2616941ac4cb4297
OJ Haugen <ohaugen@usc.edu>
2015-09-09 13:29:31 -0700

    add empty signin and signup pages, and routing to them

---
+++ server/users/userController.js
@@ -0,0 +1,35 @@
+var User  = require('./userModel.js'),
+    Q     = require('q'),
+    jwt   = require('jwt-simple');
+
+module.exports = {
+  signin: function (req, res, next) {
+    var username = req.body.username,
+        password = req.body.password;
+
+    var findUser = Q.nbind(User.findOne, User);
+    findUser({username: username})
+      .then(function (user) {
+        if (!user) {
+          next(new Error('User does not exist'));
+        } else {
+          return user.comparePasswords(password)
+            .then(function(foundUser) {
+              if (foundUser) {
+                var token = jwt.encode(user, 'secret');
+                res.json({token: token});
+              } else {
+                return next(new Error('No user'));
+              }
+            });
+        }
+      })
+      .fail(function (error) {
+        next(error);
+      });
+  },
+
+  signup: function (req, res, )
+
+
+}
