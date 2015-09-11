67a6be189a54e220440883adf0fe8db6500e9cd5
ArminSebastian <arminsebastian@gmail.com>
2015-09-11 15:57:48 -0700

    adds apiKeys.js to tracked files

---
+++ apiKeys.js
@@ -0,0 +1,19 @@
+//File for API keys
+//gitignore
+
+
+module.exports.yelpKeys = function(){
+   return {
+     consumer_key: "gRjbI1aeaADfI-UpfgMvcQ",
+     consumer_secret: "f4gBgw5Nwo9ygzFxJUZVJsuCIZ0",
+     token: "DDmyQ7zbyPHISB29Y1-YjxNqQt9zaxr6",
+     token_secret: "EoPu9WW4fprZ2iRhnix1rkvlaac"
+   };
+};
+
+module.exports.foursquareKeys = function() {
+ return {
+   client_ID: 'YATRU5X0A43SCPH20WAHTVHNDEKW0QP2L3RRMGAEVGAQFR0T',
+   client_secret: 'JH5MQQ00YZFVJTZ13XEYMA1PD031V3NUU5KPC4NI0COATNK4'
+ };
+};
